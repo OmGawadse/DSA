a985b24df421335698d76204b5f9465e2509c616
OmGawadse <omgawadse@gmail.com>
2026-01-26 13:56:16 +0530

    Added palindrome_using_recursion.java

---
+++ palindrome_using_recursion.java
@@ -0,0 +1,33 @@
+// 29.
+//Palindrome or Not.
+//logic not cleared.
+/* 
+import java.util.Scanner;
+public class palindrome_using_recursion {
+    public static void palindrome(int i, int n, int arr[]) {
+
+
+    }
+
+
+    public static void main(String[] args) {
+         try(Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the Size of Elements : ");
+            int size = sc.nextInt();                                            //size of array
+
+            int arr[] = new int[size];
+            //System.out.println(" Enter the Elements of Array : ");
+            
+            //Taking Inputs:
+            for(int i = 0; i < size; i++) {
+                System.out.println("Element at index "+ i +" : ");
+                arr[i] = sc.nextInt();                                           //input elements in array
+            }
+            palindrome(0,size,arr);
+
+        
+    }
+}
+    
+}
+*/
