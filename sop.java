67a2ce67bf21b48beda275e8b38328dd165f10e7
OmGawadse <omgawadse@gmail.com>
2026-01-26 13:56:12 +0530

    Added sop.java

---
+++ sop.java
@@ -0,0 +1,9 @@
+//1.
+//Adding two numbers
+public class sop {
+    public static void main(String[] args) {
+        int a = 5, b = 15;
+        int sum = a + b;
+        System.out.println("Sum of these numbers: " + sum);
+    }
+}
