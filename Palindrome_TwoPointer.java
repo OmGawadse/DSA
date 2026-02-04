d7b102b25797303d14da26ad6fb9a711c0d668ec
OmGawadse <omgawadse@gmail.com>
2026-02-04 23:43:21 +0530

    Palindrome Optimal Approach

---
+++ Palindrome_TwoPointer.java
@@ -0,0 +1,28 @@
+// 41. Palindrome Two Pointer
+
+public class Palindrome_TwoPointer {
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;   // not palindrome
+            }
+            left++;
+            right--;
+        }
+        return true;   // palindrome
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+
+        if (isPalindrome(str)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
