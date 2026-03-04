46e8cfbc282bcbf93fab5ec9bac3b1212e21d2a2
OmGawadse <omgawadse@gmail.com>
2026-03-04 21:14:19 +0530

    Simply Contains Dupicate

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,8 +1,5 @@
 // 50: LeetCode Problem: 217. Contains Duplicate.
 // ✅ Brute Force Approach: O(n^2) Time Complexity, O(1) Space Complexity
-// ✅ Optimal Approach (HashSet/HashMap): O(n) Time Complexity, O(n) Space Complexity
-
-import java.util.HashSet;
 
 public class ContainsDuplicate {
 
@@ -21,29 +18,12 @@ public class ContainsDuplicate {
         return false;
     }
 
-    // Optimal Approach (HashSet)
-    public static boolean containsDuplicateOptimal(int[] nums) {
-        int n = nums.length;
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int i = 0; i< n; i++) {
-            
-            if(set.contains(nums[i])) {
-                return true;
-            }
-            
-            set.add(nums[i]);
-        }
-        
-        return false;
-    }
-
     // ✅ Main Method (To Run in VS Code)
     public static void main(String[] args) {
         
         int[] arr = {1, 2, 3, 4, 1};
 
         System.out.println("Brute Force Result-O(n^2) Time Complexity, O(1) Space Complexity: " + containsDuplicateBrute(arr));
-        System.out.println("Optimal Result-O(n) Time Complexity, O(n) Space Complexity: " + containsDuplicateOptimal(arr));
+       
     }
 }
