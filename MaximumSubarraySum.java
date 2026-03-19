6a4097ccb4c04aa615daaf0f04ea44b3a36693d3
OmGawadse <omgawadse@gmail.com>
2026-03-19 23:55:39 +0530

    Maximum Subarray Sum

--- MaximumSubarraySum.java
+++ MaximumSubarraySum.java
@@ -20,6 +20,21 @@ public class MaximumSubarraySum{
         return maxi;
     }
 
+    // BETTER APPROACH: O(n^2) - Using 2 nested loops
+    public static int maxSubArrayBetter(int[] nums) {
+        int n = nums.length;
+        int maxi = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) { // Start point
+            int sum = 0; 
+            for (int j = i; j < n; j++) { // End point
+                // Optimization: Just add the current element to the previous sum
+                sum += nums[j]; 
+                maxi = Math.max(maxi, sum);
+            }
+        }
+        return maxi;
+    }
 
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
@@ -30,6 +45,9 @@ public class MaximumSubarraySum{
         int resultBrute = maxSubArrayBrute(nums);
         System.out.println("Brute Force Result (O(n^3)): " + resultBrute);
 
+        // Running Better Approach
+        int resultBetter = maxSubArrayBetter(nums);
+        System.out.println("Better Approach Result (O(n^2)): " + resultBetter);
         
         
       
