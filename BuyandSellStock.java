6ac5e2b0de3faab292ad1839ba6a7def091e2378
OmGawadse <omgawadse@gmail.com>
2026-03-22 16:47:46 +0530

    Best Time to Buy and Sell stock

--- BuyandSellStock.java
+++ BuyandSellStock.java
@@ -3,35 +3,25 @@
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
-// Example 1:
-// Input: prices = [7,1,5,3,6,4]
-// Output: 5
-// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
-
 public class BuyandSellStock {
 public static int maxProfit(int[] prices) {
 
         int maxProfit = 0; // This stores our best deal so far
-        int minPrice = prices[0]; // Assume the first day is the cheapest to start
+        int minPrice = Integer.MAX_VALUE; // Assume the first day is the cheapest to start or the lowest price we've encountered so far.
 
         // We start from the second day (index 1)
-        for (int i = 1; i < prices.length; i++) {
-            
-            // 1. Calculate potential profit if we sold today
-            // Profit = (Today's Price) - (Cheapest price we saw in the past)
-            int currentProfit = prices[i] - minPrice;
+        for (int i = 0; i < prices.length; i++) {
 
-            // 2. Update our overall maxProfit if today's deal is better
-            if (currentProfit > maxProfit) {
-                maxProfit = currentProfit;
-            }
-
-            // 3. Update our minPrice if today's price is even cheaper 
-            // than what we've seen before. This prepares us for future sales!
+            // 1. Update our minPrice if today's price is even cheaper. 
             if (prices[i] < minPrice) {
                 minPrice = prices[i];
             }
+
+            // 2. Calculate the max profit using Math.max()
+            // Compares current maxProfit against the todaysProfit if there is a deal today.
+            // todaysProfit = (Today's Price) - (Cheapest price we saw in the past)
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            
         }
         return maxProfit;
     }
@@ -57,6 +47,4 @@ Day 4 (3): Profit = $3 - 1 = 2$. maxProfit stays 4.
 Day 5 (6): Profit = $6 - 1 = 5$. maxProfit becomes 5.
 Day 6 (4): Profit = $4 - 1 = 3$. maxProfit stays 5.
 Result: 5.
-
-
 */
