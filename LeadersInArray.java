d0a09bb0a5e8e33280a7fbcbceb6c81b0a770a59
OmGawadse <omgawadse@gmail.com>
2026-04-09 18:19:10 +0530

    Leaders in the Array

--- LeadersInArray.java
+++ LeadersInArray.java
@@ -1,13 +1,15 @@
 // 59. Leaders in an array
 // An element is a leader if it is greater than all the elements to its right side. 
 // The rightmost element is always a leader.
+// Time Complexity: O(n^2) for brute force.
+// Time Complexity: O(n) for optimal approach.
 
 import java.util.*;
 
 public class LeadersInArray {
 
     // Function to find leaders in array using brute force
-    public static List<Integer> leaders(int[] arr) {
+    public static List<Integer> BruteForceApproach(int[] arr) {
         List<Integer> result = new ArrayList<>(); // to store leaders
         int n = arr.length;
 
@@ -26,16 +28,30 @@ public class LeadersInArray {
                     break; // no need to check further
                 }
             }
-
             // Step 3: If still true → it is a leader
             if(Leader) {
                 result.add(arr[i]);
             }
         }
-
         return result; // return list of leaders
     }
- 
+
+    public static List<Integer> OptimalApproach(int[] arr) {
+        List<Integer> result = new ArrayList<>(); 
+        int n = arr.length;
+        int max = arr[n-1]; // rightmost element is always a leader
+        result.add(max); // add rightmost element to result
+
+        // traverse backwards:
+        for(int i = n-2; i >=0; i--){
+            if(arr[i] > max) { 
+                max = arr[i]; // update max
+                result.add(max); // add new leader to result                
+            }
+        }
+        return result;
+    }
+        
 public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
 
@@ -50,13 +66,18 @@ public static void main(String[] args) {
             }
 
             // Call function to get leaders
-            List<Integer> result = leaders(arr);
+            List<Integer> result = BruteForceApproach(arr);
+            List<Integer> returnresult = OptimalApproach(arr);
 
-            System.out.println("Leaders in array:");
+            System.out.println("Leaders in array (Brute Force):");
             for(int num : result) {
                 System.out.print(num + " ");
             }
+            System.out.println();
+            System.out.println("Leaders in array (Optimal):");
+            for(int num : returnresult) {
+                System.out.print(num + " ");
+            }
         }
-
     }
 }
