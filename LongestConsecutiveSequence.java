38f9860319d42d402de8eeedbaa3c9d7a61d5cc7
OmGawadse <omgawadse@gmail.com>
2026-04-09 23:27:12 +0530

    Longest Consecutive Sequence

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,48 @@
+// 60. Leetcode 128. Longest Consecutive Sequence
+// Time Complexity: O(n^2) for brute force approach.
+// Time Complexity: O(n) for optimal approach using HashSet.
+
+
+public class LongestConsecutiveSequence {
+
+    // Method inside a method is not allowed in Java, so we define linearSearch as a separate method.
+    public static boolean linearSearch(int[] arr, int target) {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == target) {
+                return true; // found target
+            }
+        }
+        return false; // target not found
+    }
+
+    public static int BruteForceApproach(int[] arr) {
+        int n = arr.length;
+
+        int longest = 1; // at least one element exists
+        
+        for(int i = 0; i < n; i++){
+            int x = arr[i]; // current element
+            int count = 1;  // sequence starts with 1
+
+            // check for next consecutive elements
+            while(linearSearch(arr, x + 1)) {
+                x++;      // move to next element
+                count++;  // increase sequence length
+            }
+
+            // update longest sequence
+            longest = Math.max(longest, count);
+        }
+
+        return longest; // return answer
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 5, 12, 3, 55, 4, 11, 2, 1, 6};
+
+        int result = BruteForceApproach(arr);
+
+        System.out.println("Longest Consecutive Length Using Brute Force : " + result);
+    }
+}
