249e33af6987ee053a52ae3df86349d856025893
OmGawadse <omgawadse@gmail.com>
2026-04-10 21:42:12 +0530

    Longest Consecutive Sequence

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -2,7 +2,7 @@
 // Time Complexity: O(n^2) for brute force approach.
 // Time Complexity: O(n) for optimal approach using HashSet.
 
-
+import java.util.Arrays;    
 public class LongestConsecutiveSequence {
 
     // Method inside a method is not allowed in Java, so we define linearSearch as a separate method.
@@ -37,12 +37,40 @@ public class LongestConsecutiveSequence {
         return longest; // return answer
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////// Better Approach
+    
+    public static int BetterApproach(int[] arr) {
+        int n = arr.length;
+
+        int longest = 1; // at least one element exists & will give us answer
+        int count = 1;   // sequence starts with 1
+
+        Arrays.sort(arr);
+
+        for (int i=1; i<n; i++) {
+            if (arr[i] == arr[i-1]+1) {
+                count++; // current element is consecutive, increase count
+            }
+            else if(arr[i] == arr[i-1]){
+                continue; // skip duplicates
+            }
+            else{
+                count = 1; // reset count for new sequence
+            }
+            longest = Math.max(longest, count); // update longest sequence
+        }
+        return longest; // return answer
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {10, 5, 12, 3, 55, 4, 11, 2, 1, 6};
 
         int result = BruteForceApproach(arr);
+        int resultB = BetterApproach(arr);  
 
         System.out.println("Longest Consecutive Length Using Brute Force : " + result);
+        System.out.println("Longest Consecutive Length Using Better Approach : " + resultB);
+        
     }
 }
