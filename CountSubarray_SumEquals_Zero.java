db71eafdf55c1492e3ce5359efc6105ec0a7a746
OmGawadse <omgawadse@gmail.com>
2026-05-19 14:23:40 +0530

    Count the subarrays that's sum equal to K

--- CountSubarray_SumEquals_Zero.java
+++ CountSubarray_SumEquals_Zero.java
@@ -1,3 +1,6 @@
+
+import java.util.HashMap;
+
 // 64 . Count the number of subarrays that sum to a given value k.
 // LeetCode 560 : Subarray Sum Equals K
 
@@ -52,6 +55,34 @@ public class CountSubarray_SumEquals_Zero {
     }
 
 
+    // OPTIMAL APPROACH -------> Time Complexity  : O(n)    -     Space Complexity : O(n)
+
+    public static int OptimalApproach(int[] nums, int k){
+
+        int n = nums.length;
+        int prefixsum = 0;
+        int count = 0;
+
+        // HashMap to store frequency of prefix sums
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // Base case: prefix sum of 0 occurs once.
+
+        for(int i = 0; i < n; i++) {
+
+            prefixsum += nums[i];
+
+            // Check if there is a prefix sum that equals current prefix sum - k
+            int check = prefixsum - k;
+            if(map.containsKey(check)){  
+                count += map.get(check); // Increment count by the frequency of the required prefix sum
+            }
+
+            // Update the frequency of the current prefix sum
+            map.put(prefixsum, map.getOrDefault(prefixsum, 0) + 1);
+        }
+        return count;
+    }
+
 // ================================================================================================================
 
     public static void main(String[] args) {
@@ -61,9 +92,12 @@ public class CountSubarray_SumEquals_Zero {
 
         int bruteAnswer = BruteForceApproach(nums, k);
         int betterAnswer = BetterApproach(nums, k);
+        int optimalAnswer = OptimalApproach(nums, k);
 
         System.out.println("Brute Force Answer : " + bruteAnswer);
         System.out.println("Better Approach Answer : " + betterAnswer);
+        System.out.println("Optimal Approach Answer : " + optimalAnswer);
+
         System.err.println("output = 3");
     }
 }
