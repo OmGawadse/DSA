de340e81cc286f4a3099be155ac76e51a1b6b0bc
OmGawadse <omgawadse@gmail.com>
2026-08-06 23:39:40 +0530

    Majority Element II > N/3

--- MajorityElementII.java
+++ MajorityElementII.java
@@ -1,9 +1,11 @@
 // 66. Majority Element II(N/3)
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MajorityElementII {
+    // 1. Brute Force Approach (O(n^2) Time | O(1) Space)
     public static List<Integer> BruteForce(int[] nums) {
 
         List<Integer> ans = new ArrayList<>(); // This list will store the final majority elements.
@@ -11,7 +13,8 @@ public class MajorityElementII {
 
         for (int i = 0; i < n; i++) {
 
-            // If we've already added this number in List, don't count it again. This avoids Duplicate.
+            // Skip the element if it's already in the answer list.
+            // This prevents adding duplicate majority elements.        
             if (ans.contains(nums[i])) {  
                 continue;
             }
@@ -29,6 +32,34 @@ public class MajorityElementII {
         return ans;
     }
 
+
+    // 2. Better Approach using Hashing (O(n) Time | O(n) Space)
+    public static List<Integer> BetterApproach(int nums[]){
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+
+       // Step 1: Count the frequency of every element
+        for (int i = 0; i < n; i++) {
+
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        // Step 2: Traverse the HashMap.
+        // If an element appears more than n/3 times,
+        // add it to the answer.
+        for (int x : map.keySet()) {
+
+            if (map.get(x) > n / 3) {
+                ans.add(x);
+            }
+        }return ans;
+    }
+
     public static void main(String[] args) {
 
         int[] nums = {3, 2, 3};
@@ -36,5 +67,8 @@ public class MajorityElementII {
         List<Integer> result = BruteForce(nums);
 
         System.out.println("Majority Elements: " + result);
-    }
+
+        List<Integer> answer = BetterApproach(nums);
+        System.out.println("Majority Elements: " + answer);
+        }
 }
