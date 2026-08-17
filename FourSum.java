b6e608945d0565ad15c006bd79a563a90435c7ef
OmGawadse <omgawadse@gmail.com>
2026-08-17 20:49:27 +0530

    4 Sum

--- FourSum.java
+++ FourSum.java
@@ -69,6 +69,60 @@ public class FourSum {
         }
         // Convert Set<List<Integer>> into List<List<Integer>>
         return new ArrayList<>(ans);
+    } 
+
+
+    public  static List<List<Integer>> OptimalApproach(int[] nums, int target) {
+        int n = nums.length;
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+
+            // Skip duplicate first numbers
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                // Skip duplicate second numbers
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];     // Use long to avoid integer overflow
+
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+
+                        // Skip duplicate left values
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+
+                        // Skip duplicate right values
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 
 
@@ -83,6 +137,9 @@ public class FourSum {
 
         List<List<Integer>> result2 = BetterApproach(nums, target);
         System.out.println(result2);
+
+        List<List<Integer>> answer = OptimalApproach(nums, target);
+        System.out.println(answer);
 
 
     }
