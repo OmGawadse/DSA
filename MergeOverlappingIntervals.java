2ec5475ffaf9c176b5d2db32bb43d06ee969ebb0
OmGawadse <omgawadse@gmail.com>
2026-08-20 21:11:44 +0530

    Merge Overlapping Arrays

--- MergeOverlappingIntervals.java
+++ MergeOverlappingIntervals.java
@@ -1,34 +1,41 @@
 // 70. Merge Overlapping Intervals
 // Leetcode: 56
+
 import java.util.*;
 
 public class MergeOverlappingIntervals {
 
-    public static int[][] OptimalApproach(int[][] intervals) {
+    public static int[][] BruteForceApproach(int[][] intervals) {
 
         int n = intervals.length;
 
         List<int[]> ans = new ArrayList<>();
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); // This is the way of sorting a 2D array in Java.
 
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        int start = intervals[0][0];  // Starting Value of 1st subarray/ interval.
+        int end = intervals[0][1];   // Ending value of 1st subarray/ interval.
 
         for (int i = 1; i < n; i++) {
 
             if (intervals[i][0] <= end) {
-                end = Math.max(end, intervals[i][1]);
-            } else {
+                // current start <= previous end, of Overlapping happens. what about the ending part/value ?
+                // Lets check about the ending part: 
+                end = Math.max(end, intervals[i][1]); //if greater that previous end, Simply update it.
+            }
+
+            else {
+                // No overlap → So it can be a new Interval -> store the current interval.
                 ans.add(new int[] {start, end});
 
-                start = intervals[i][0];
+                start = intervals[i][0];      // Update the Start & End to Start a new interval.
                 end = intervals[i][1];
             }
         }
-
+        // Add the last interval to the answer list bcoz there is no next interval to check for overlap.
         ans.add(new int[] {start, end});
 
+        // Convert my List of integer arrays into a 2D integer array
         return ans.toArray(new int[ans.size()][]);
     }
 
@@ -36,13 +43,16 @@ public class MergeOverlappingIntervals {
 
         int[][] intervals = {
             {1, 3},
-            {2, 6},
-            {8, 10},
-            {15, 18}
+            {2, 5},
+            {7, 9},
+            {8, 11},
+            {15, 18},
+            {121, 237}
         };
 
-        int[][] result = OptimalApproach(intervals);
+        int[][] result = BruteForceApproach(intervals);
 
+        // Print the result
         for (int[] interval : result) {
             System.out.println(
                 "[" + interval[0] + ", " + interval[1] + "]"
