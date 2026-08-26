7304c077f5cefe60dd0b440b5659a33c94bfadd4
OmGawadse <omgawadse@gmail.com>
2026-08-26 20:14:05 +0530

    Count Inversionsssss

--- Count_Inversions.java
+++ Count_Inversions.java
@@ -1,3 +1,7 @@
+// 71. Count Inversions in an array
+// An inversion is a pair of indices (i, j) such that i < j and arr[i] > arr[j].
+// Brute Force Approach: O(n^2) time complexity & O(1) space complexity
+// Just compare each element with all the elements after it and count the number of inversions.
 
 public class Count_Inversions {
 
