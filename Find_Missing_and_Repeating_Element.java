c0af5f023f6e808caf24263dbd0c82a396028068
OmGawadse <omgawadse@gmail.com>
2026-08-27 20:46:45 +0530

    Find Missing & Repeating Element

--- Find_Missing_and_Repeating_Element.java
+++ Find_Missing_and_Repeating_Element.java
@@ -34,13 +34,36 @@ public class Find_Missing_and_Repeating_Element {
         return new int[]{missing, repeating};       // Return Both Answers:- result[0] = missing number & result[1] = repeating number
     }
 
+
+    public static int[] BetterApproach(int[] nums) {
+        int n = nums.length;
+        int missing = -1;
+        int repeating = -1;
+
+        // Create a frequency array. This store the frequency of each number.
+        int[] freq = new int[n + 1]; 
+
+        for(int i=0; i<n; i++) {
+            freq[nums[i]]++;
+        }
+
+        for(int i = 1; i < n; i++){
+            if(freq[i] == 0) missing = i;          // If the number appears 0 times, it is the missing number
+            if(freq[i] > 1) repeating = i;         // If the number appears more than once, it is the repeating number
+        }
+        return new int[]{missing, repeating};
+    }
+
     public static void main(String args[]) {
 
         int[] nums = {4, 3, 6, 2, 1, 1};
 
         int[] result = BruteForceApproach(nums);
-
         System.out.println("Missing: " + result[0]);        // result[0] contains the missing number
         System.out.println("Repeating: " + result[1]);      // result[1] contains the repeating number  
+
+        int[] result2 = BetterApproach(nums);
+        System.out.println("Missing: " + result2[0]);       
+        System.out.println("Repeating: " + result2[1]);
     }
 }
