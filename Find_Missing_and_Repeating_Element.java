66945aec2486f7378825a174d21d9832a4bdcfdd
OmGawadse <omgawadse@gmail.com>
2026-08-27 23:55:15 +0530

    Find Missing & Repeating Element

--- Find_Missing_and_Repeating_Element.java
+++ Find_Missing_and_Repeating_Element.java
@@ -19,19 +19,12 @@ public class Find_Missing_and_Repeating_Element {
             // Count frequency of the number i in the array
             for(int j = 0; j < n; j++) {
 
-                if(nums[j] == i) {
-                    count++;
-                }
+                if(nums[j] == i) count++;
             }
-            if(count == 0) {            // If the number appears 0 times, 
-                missing = i;            // it is the missing number
-            }
-
-            if(count > 1) {             // If the number appears more than once,
-                repeating = i;          // it is the repeating number
-            }
+            
+            if(count == 0) missing = i;           // If the number appears 0 times, it is the missing number
+            if(count > 1) repeating = i;           // If the number appears more than once,it is the repeating number
         }
-
         return new int[]{missing, repeating};       // Return Both Answers:- result[0] = missing number & result[1] = repeating number
     }
 
@@ -60,11 +53,11 @@ public class Find_Missing_and_Repeating_Element {
         int[] nums = {4, 3, 6, 2, 1, 1};
 
         int[] result = BruteForceApproach(nums);
-        System.out.println("Missing: " + result[0]);        // result[0] contains the missing number
-        System.out.println("Repeating: " + result[1]);      // result[1] contains the repeating number  
+        System.out.println("Missing Number from the Sequence: " + result[0]);        // result[0] contains the missing number
+        System.out.println("Repeating Number from the Sequence: " + result[1]);      // result[1] contains the repeating number  
 
         int[] result2 = BetterApproach(nums);
-        System.out.println("Missing: " + result2[0]);       
-        System.out.println("Repeating: " + result2[1]);
+        System.out.println("Missing Number from the Sequence: " + result2[0]);       
+        System.out.println("Repeating Number from the Sequence: " + result2[1]);
     }
 }
